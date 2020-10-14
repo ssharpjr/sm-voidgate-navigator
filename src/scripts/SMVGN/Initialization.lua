9300e5b0a4d952edd036566f600c9f842e12f8b6
Stacey Sharp <ssharpjr@gmail.com>
2020-10-13 23:57:44 -0400

    updated comments

--- src/scripts/SMVGN/Initialization.lua
+++ src/scripts/SMVGN/Initialization.lua
@@ -1,10 +1,15 @@
--- Starmourn Voidgate Navigator
-
--- Credits:
--- - Original code from Maruna's post on the Starmourn forums.
---   - https://forums.starmourn.com/discussion/760/voidgates-for-lazy-people
---
--- - Clean up and packaging: Daikojun (github.com/ssharpjr)
+---------------------------------------------------------------------------
+-- SMVGN - Starmourn Voidgate Navigator                                  --
+--                                                                       --
+-- A Mudlet Package for navigating voidgates in Starmourn                --
+-- Game: Starmourn (http://www.starmourn.com)                            --
+-- Client: Mudlet (http://www.mudlet.org)                                --
+--                                                                       --
+-- Package Author: Daikojun (github.com/ssharpjr)                        --
+-- Source: (https://github.com/ssharpjr/sm-voidgate-navigator)           --
+-- Original code and data from Maruna's post on the Starmourn forums     --
+-- https://forums.starmourn.com/discussion/760/voidgates-for-lazy-people --
+---------------------------------------------------------------------------
 
 smvgn = smvgn or {}
 
